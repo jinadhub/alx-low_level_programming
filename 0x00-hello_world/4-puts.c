297dda5afebf77741f9a45ddb06aee8ce8e686b9
jinadhub <pydevop1@gmail.com>
2023-09-07 23:06:09 +0200

    Added 4-puts
    The script uses the puts() function to print a script to the stdout

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*Description: main - printing strings using the puts function
+* Return: 0 if successful
+*/
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
