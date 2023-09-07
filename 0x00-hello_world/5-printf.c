d495210cc8ac4ca41fc4654d237e1a85b4e40477
jinadhub <pydevop1@gmail.com>
2023-09-07 23:08:17 +0200

    Added the 5-printf.c file
    The script uses the printf function to print a string to the stdout

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*Description: main - printing strings using the printf function
+*
+*Return: return (0) if successful
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
